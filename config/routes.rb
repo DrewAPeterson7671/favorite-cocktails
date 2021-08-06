0b23bfcc600bb5b96b61922425a1005509605dcd
Drew Peterson <drew.a.peterson@hotmail.com>
2021-08-05 20:42:26 -0700

    fix issue setting up users for routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'cocktails#index'
-  resources :users do
-    resources :cocktails
-  end
+  resources :cocktails
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
