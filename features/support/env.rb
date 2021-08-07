e813b10498760388fc58919a9aaa24663297643d
Drew Peterson <drew.a.peterson@hotmail.com>
2021-08-07 14:36:56 -0700

    set up email spec gem

--- features/support/env.rb
+++ features/support/env.rb
@@ -6,6 +6,10 @@
 
 require 'cucumber/rails'
 
+require 'email_spec' # add this line if you use spork
+require 'email_spec/cucumber'
+
+
 # frozen_string_literal: true
 
 # Capybara defaults to CSS3 selectors rather than XPath.
