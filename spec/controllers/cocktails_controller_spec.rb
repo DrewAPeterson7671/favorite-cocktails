c4da31a1fb5383c1eba28776655b82f7f76ee7a4
Drew Peterson <drew.a.peterson@hotmail.com>
2021-08-13 11:24:34 -0700

    New and Create controller methods tested and updated, tested failed and then passed with adjustment

--- spec/controllers/cocktails_controller_spec.rb
+++ spec/controllers/cocktails_controller_spec.rb
@@ -1,4 +1,5 @@
 require "rails_helper"
+require 'pry'
 
 RSpec.describe CocktailsController do
 
@@ -23,5 +24,41 @@ RSpec.describe CocktailsController do
     end
     
   end
+
+  describe 'POST #create' do
+    let(:cocktail) { FactoryBot.build_stubbed(:cocktail) } 
+    let(:params) { { :name => "Martini", :ingredients => "Booze and an olive" } }
+
+    before do
+      allow(cocktail).to receive(:save)
+      allow(user).to receive_message_chain(:cocktails, :build).and_return(cocktail)
+    end
+
+    it "saves the cocktail" do
+      post :create, :params => { :cocktail => params }
+      
+      expect(cocktail).to have_received(:save)
+    end
+    
+    context 'when the cocktail is successfully saved' do
+      
+      before do
+        allow(cocktail).to receive(:save).and_return(true)
+        
+        post :create, :params => { :cocktail => params }
+      end
+      
+      it 'redirects to the cocktail show page' do
+        expect(response).to redirect_to(cocktail_path(cocktail))
+      end
+  
+      it 'redirects to the cocktail show page' do
+        expect(flash[:notice]).to eq('Cocktail was successfully created.')
+      end
+    end
+
+  end
+  
+  
 
 end
