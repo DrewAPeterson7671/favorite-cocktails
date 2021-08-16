55dcc12eef16fad4960ebe8b75a17fe63f5ef2c1
Drew Peterson <drew.a.peterson@hotmail.com>
2021-08-16 16:38:46 -0700

    Add pages controller for api search page

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
 
   devise_for :users
   
+  get '/search' => 'pages#search', :as => 'search_page'
+
   root to: 'cocktails#index'
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
