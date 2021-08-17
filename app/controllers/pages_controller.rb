22c0bfca160d101b716e5d63001ae89893e89002
Drew Peterson <drew.a.peterson@hotmail.com>
2021-08-17 16:05:26 -0700

    Search form is successfully making calls to the api using the search form parameters

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,9 @@ class PagesController < ApplicationController
 
   def search
     @search = params[:search]
-
+    api_call = Drinks.new(@search)
+    response = api_call.drink_name
+    @drinks = JSON(response.body)
 
     render :search
   end
