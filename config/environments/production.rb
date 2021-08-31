8c4fe28ef0975ac462704795e131bbf7fccc6fcc
Drew Peterson <drew.a.peterson@hotmail.com>
2021-08-31 15:11:36 -0700

    Set up Action Mailer for registration confirmation

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -93,5 +93,17 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.active_storage.service = :amazon
+  config.action_mailer.default_url_options = { :host => 'gmail.com' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :tls => true,
+    :address => "smtp.gmail.com",
+    :port => "587",
+    :domain => "gmail.com"
+    :authentication => :login,
+    :user_name => ENV['EMAIL_USERNAME'],
+    :password => ENV['EMAIL_PASSWORD']
+  }
+
 
 end
