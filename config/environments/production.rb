90a3f66c678cd34d60da2794aff8e52d7d2652c0
Drew Peterson <drew.a.peterson@hotmail.com>
2021-08-31 15:53:23 -0700

    Configuring gmail for confirmation emails again

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -100,7 +100,7 @@ Rails.application.configure do
     :address => "smtp.gmail.com",
     :port => "587",
     :domain => "gmail.com"
-    :authentication => "plain",
+    :authentication => :plain,
     :user_name => ENV['EMAIL_USERNAME'],
     :password => ENV['EMAIL_PASSWORD']
   }
