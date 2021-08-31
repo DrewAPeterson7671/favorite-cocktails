554aa2071e4cf5a85e3663c0661442c2004c394d
Drew Peterson <drew.a.peterson@hotmail.com>
2021-08-31 16:30:01 -0700

    Attempting to configure production action mailer for gmail 2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -93,17 +93,17 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.active_storage.service = :amazon
-  
+
   # config.action_mailer.default_url_options = { :host => 'gmail.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    :address => "smtp.gmail.com",
-    :port => 587,
-    :domain => "gmail.com"
-    :user_name => ENV['EMAIL_USERNAME'],
-    :password => ENV['EMAIL_PASSWORD']
-    :authentication => "plain",
-    :enable_starttls_auto => true
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: "gmail.com",
+    user_name: ENV['EMAIL_USERNAME'],
+    password: ENV['EMAIL_PASSWORD'],
+    authentication: "plain",
+    enable_starttls_auto: true
   }
 
 
