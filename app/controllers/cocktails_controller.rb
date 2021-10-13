6ee9146099174e812f1dd2b8e6ba451bf68d4009
Drew Peterson <drew.a.peterson@hotmail.com>
2021-10-13 14:14:03 -0700

    Fix redirect on ajax delete that did not redirect to index with notice it was successfully destroyed

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -81,7 +81,7 @@ class CocktailsController < ApplicationController
   def destroy
     @cocktail.destroy
     respond_to do |format|
-      format.js
+      format.js { redirect_to cocktails_url, notice: "Cocktail was successfully destroyed." }
       format.html { redirect_to cocktails_url, notice: "Cocktail was successfully destroyed." }
       format.json { head :no_content }
     end
