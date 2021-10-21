e6164ab6b7c77a204b7b6c00cf6c8c0ee23b30e2
Drew Peterson <drew.a.peterson@hotmail.com>
2021-10-20 17:49:42 -0700

    Set up sendgrid and also using the new domain

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -93,18 +93,15 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.active_storage.service = :amazon
+  
 
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default_url_options = { :host => 'https://myfavorite-cocktails.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    address: "smtp.gmail.com",
-    port: 587,
-    domain: "heroku.com",
-    user_name: ENV['EMAIL_USERNAME'],
-    password: ENV['EMAIL_PASSWORD'],
-    authentication: "plain",
-    enable_starttls_auto: true
-  }
+  config.action_mailer.perform_deliveries = true
+
+
 
 
 end
