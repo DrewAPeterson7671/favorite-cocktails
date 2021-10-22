d72c0f9afdfe5e20a6368c32f525010f78476a7e
Drew Peterson <drew.a.peterson@hotmail.com>
2021-10-22 12:29:38 -0700

    change sendgrid api mailer approach to a gem

--- app/mailers/devise_mailer.rb
+++
@@ -1,8 +0,0 @@
-class MyDeviseMailer < Devise::Mailer
-  helper :application
-  include Devise::Controllers::UrlHelpers
-
-  def reset_password_instructions(record, token, opts={})
-    SendgridWebMailer.send_some_email(record, token).deliver_later
-  end
-end
--- config/application.rb
+++ config/application.rb
@@ -29,5 +29,12 @@ module MyCocktails
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+
+    config.action_mailer.delivery_method = :sendgrid_actionmailer
+    config.action_mailer.sendgrid_actionmailer_settings = {
+      api_key: ENV['SENDGRID_API_KEY'],
+      raise_delivery_errors: true
+    }
+
   end
 end
